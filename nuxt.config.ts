f81b4af22cf708cfe7949ca9fa14ba5a4cef6ac3
Nicole Li <nicole.li@elementari.com>
2022-08-23 14:53:56 +0200

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,14 +3,17 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
     ssr: false,
-    hooks: {
-        'vite:extendConfig' (config, { isClient }) {
-          if (process.env.NODE_ENV !== 'development' && isClient) {
-            config.build.rollupOptions.output.chunkFileNames = '[name]-[hash].js'
-            config.build.rollupOptions.output.entryFileNames = '[name]-[hash].js'
-          }
-        }
-      },
+    nitro: {
+      preset: 'service-worker',
+    }
+    // hooks: {
+    //     'vite:extendConfig' (config, { isClient }) {
+    //       if (process.env.NODE_ENV !== 'development' && isClient) {
+    //         config.build.rollupOptions.output.chunkFileNames = '[name]-[hash].js'
+    //         config.build.rollupOptions.output.entryFileNames = '[name]-[hash].js'
+    //       }
+    //     }
+    //   },
     css: [
         "vuetify/lib/styles/main.sass"
     ],
