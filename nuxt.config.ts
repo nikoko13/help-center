8292756efb52a1e10a0681572e9405851c25873b
Nicole Li <nicole.li@elementari.com>
2022-08-23 14:58:44 +0200

    Update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
     ssr: false,
     nitro: {
       preset: 'service-worker',
-    }
+    },
     // hooks: {
     //     'vite:extendConfig' (config, { isClient }) {
     //       if (process.env.NODE_ENV !== 'development' && isClient) {
